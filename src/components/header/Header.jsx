4e050577bf17579f80c8572985525bc5ac5cfbc2
Apurva Milind Gaikwad <gaikwadapurva65@gmail.com>
2023-04-30 23:34:48 +0530

    header change and css colour change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/me1.png';
+// import ME from '../../assets/me1.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
@@ -14,9 +14,9 @@ const Header = () => {
         <CTA />
         <HeaderSocials />
 
-        <div className="me">
+        {/* <div className="me">
           <img src={ME} alt='me' />
-        </div>
+        </div> */}
 
         <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
